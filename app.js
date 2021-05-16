80516c99f942c4c11405334a879015cf2ed40921
Chintan Bhalodiya <53387537+Chintan-24@users.noreply.github.com>
2021-05-16 22:45:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var txtInput = document.querySelector("#translate-input");
+var btnTranslate = document.querySelector("#translate-button");
+var outputDiv = document.querySelector("#translate-output");
+var serverURL = "https://api.funtranslations.com/translate/yoda.json";
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("Error Occured!", error)
+    alert("Error Occured! Maybe FORCE is not with you :(");
+}
+
+function clickHandler(){
+
+    var inputText = txtInput.value; //taking input
+
+//calling server for processiong
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json =>{
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
